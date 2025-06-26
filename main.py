52c604eed315b22aa5ff32cde2ed6e25391eea3b
gian-guerra <gian.guerra.engineer@gmail.com>
2025-06-25 22:09:09 -0500

    refactor display lines and remove double instructions

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ INSTRUCTIONS = "Instructions: [r]epeat | [n]ext | [q]uit | [v]record and compare
 def printMultipleLines(lines):
     for _, line in enumerate(lines):
         print(line)
-    print(INSTRUCTIONS)
     
 def shadowing_session(script_lines, audio_segments, pronunciation_data, mode):
     print("\n Shadowing Session Started ")
@@ -31,9 +30,12 @@ def shadowing_session(script_lines, audio_segments, pronunciation_data, mode):
         while True:
             printMultipleLines(toPrint)
             nextPrintLines = [f"\n📢 {line}"]
-
             play(audio_segments[index])
 
+            ipaDisplay = nextPrintLines.append(f"IPA: {pronunciation.get('ipa', 'N/A')}")
+            stressDisplay = nextPrintLines.append(f"Stress: {pronunciation.get('stress', 'N/A')}")
+            linkingDisplay = nextPrintLines.append(f"Linking:\n{pronunciation.get('linking', 'N/A')}")
+            
             command = input(INSTRUCTIONS).strip().lower()
             if command == "n":
                 break
@@ -48,16 +50,16 @@ def shadowing_session(script_lines, audio_segments, pronunciation_data, mode):
                 record.save_and_play_recording(audio_data, sr)
                 break
             elif command == "i":
-                nextPrintLines.append(f"IPA: {pronunciation.get('ipa', 'N/A')}")
+                nextPrintLines.append(ipaDisplay)
             elif mode == "lines" and command in ("s", "i", "l", "a"):
                 if command == "s":
-                    nextPrintLines.append(f"Stress: {pronunciation.get('stress', 'N/A')}")
+                    nextPrintLines.append(stressDisplay)
                 elif command == "l":
-                    nextPrintLines.append(f"Linking:\n{pronunciation.get('linking', 'N/A')}")
+                    nextPrintLines.append(linkingDisplay)
                 elif command == "a":
-                    nextPrintLines.append(f"Stress: {pronunciation.get('stress', 'N/A')}")
-                    nextPrintLines.append(f"IPA: {pronunciation.get('ipa', 'N/A')}")
-                    nextPrintLines.append(f"Linking:\n{pronunciation.get('linking', 'N/A')}")
+                    nextPrintLines.append(stressDisplay)
+                    nextPrintLines.append(ipaDisplay)
+                    nextPrintLines.append(linkingDisplay)
             else:
                 print("Invalid input. Please try again.")
             toPrint = nextPrintLines
