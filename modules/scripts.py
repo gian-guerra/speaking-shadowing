ee994365650b12528155d0c65b51b36687184d7d
gian-guerra <gian.guerra.engineer@gmail.com>
2025-07-02 23:09:18 -0500

    include colors for ipa lines

--- modules/scripts.py
+++ modules/scripts.py
@@ -3,6 +3,7 @@ import re
 RESET = "\033[0m"
 BOLD = "\033[1m"
 BLUE = "\033[94m"
+GREEN = "\033[92m"
 
 def format_line(line):
     return re.sub(r"\{(.*?)\}", f"{BOLD}{BLUE}\\1{RESET}", line)
@@ -12,7 +13,7 @@ def load_script(script_path, mode="lines", ipa_path=None):
         with open(script_path, "r", encoding="utf-8") as f1, open(ipa_path, "r", encoding="utf-8") as f2:
             script_lines = [line.strip() for line in f1 if line.strip()]
             ipa_lines = [line.strip() for line in f2 if line.strip()]
-            combined_lines = [f"{format_line(s)}\n{ipa}" for s, ipa in zip(script_lines, ipa_lines)]
+            combined_lines = [f"{format_line(s)}\n{GREEN}{ipa}{RESET}" for s, ipa in zip(script_lines, ipa_lines)]
             return ["\n".join(combined_lines)]
     else:
         with open(script_path, "r", encoding="utf-8") as f:
